49aed06ba737574a688c8769a5c09e58a8502b1f
atomjoy <atomjoy.official@gmail.com>
2023-06-23 11:24:36 +0200

    Add files via upload

---
+++ resources/views/proton/password.blade.php
@@ -0,0 +1,70 @@
+<x-proton.email title="{{ __('Activation') }}" locale="pl">
+	<x-slot:style>
+		<style>
+			.proton-flex img {
+				margin: 10px;
+				max-width: 15%;
+				width: 40px;
+			}
+		</style>
+	</x-slot:style>
+
+	<x-proton.tr>
+		<x-proton.td>
+			<x-proton.margin />
+
+			<center>
+				<h1> @lang(config('proton.email.message.welcome', 'Welcome')) {{ $user?->name ?? '' }}! </h1>
+				<img src="https://proton.me/images/social/proton-mail-og.png" alt="Image">
+			</center>
+
+			<h2> @lang('Password') </h2>
+			<p>@lang(config('proton.email.message.reset_password', 'This is your new password.'))</p>
+		</x-proton.td>
+	</x-proton.tr>
+
+	<x-proton.tr>
+		<x-proton.td>
+			<x-proton.code>
+				{{ $password ?? 'INVALIDPASS'}}
+			</x-proton.code>
+		</x-proton.td>
+	</x-proton.tr>
+
+	<x-proton.tr>
+		<x-proton.td>
+			<h3> @lang('Regards') </h3>
+			<p> @lang('Have a nice day!')</p>
+		</x-proton.td>
+	</x-proton.tr>
+
+	<x-proton.tr>
+		<x-proton.td>
+			<x-proton.divider />
+
+			<center>
+				<span style="color: #706d6b;"> © 2023 Proton Switzerland </span>
+			</center>
+
+			<x-proton.flex>
+				<a href="https://proton.me" class="proton-link">
+					<img src="https://img.icons8.com/?size=64&id=LPcVDft9Isqt&format=png" alt="Image">
+				</a>
+
+				<a href="https://proton.me" class="proton-link">
+					<img src="https://img.icons8.com/?size=64&id=LPcVDft9Isqt&format=png" alt="Image">
+				</a>
+
+				<a href="https://proton.me" class="proton-link">
+					<img src="https://img.icons8.com/?size=64&id=LPcVDft9Isqt&format=png" alt="Image">
+				</a>
+
+				<a href="https://proton.me" class="proton-link">
+					<img src="https://img.icons8.com/?size=64&id=LPcVDft9Isqt&format=png" alt="Image">
+				</a>
+			</x-proton.flex>
+
+			<x-proton.margin />
+		</x-proton.td>
+	</x-proton.tr>
+</x-proton.email>
