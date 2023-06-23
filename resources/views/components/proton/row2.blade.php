4f5fc8065a2fd6101dfd76a3832cd75be0b0b626
atomjoy <atomjoy.official@gmail.com>
2023-06-23 14:55:07 +0200

    Update row2.blade.php

--- resources/views/components/proton/row2.blade.php
+++ resources/views/components/proton/row2.blade.php
@@ -9,10 +9,10 @@
 </x-proton.row2>
 --}}
 <x-proton.tr>
-	<x-proton.td colspan="" style="padding-right: 10px;">
+	<x-proton.td colspan="3" style="padding-right: 10px; width: 50%;">
 		{{ $slot1 }}
 	</x-proton.td>
-	<x-proton.td colspan="" style="padding-left: 10px;">
+	<x-proton.td colspan="3" style="padding-left: 10px; width: 50%;">
 		{{ $slot2 }}
 	</x-proton.td>
 </x-proton.tr>
