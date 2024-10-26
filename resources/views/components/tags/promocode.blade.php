52dada6894e8ede0ebcb0d415ea550592daca11c
Atomjoy <atomjoy.official@gmail.com>
2024-10-26 14:37:04 +0200

    Add files via upload

--- resources/views/components/tags/promocode.blade.php
+++ resources/views/components/tags/promocode.blade.php
@@ -1,5 +1,5 @@
 <x-proton::tags.div type="promocode" {{ $attributes }}>
     <div 
     style="display: inline; float: left; padding: 10px 0px;">{{ __('Promotional code:') }} <strong>{{ $slot }}</strong></div> 
-    <img src="{{ $url ?? 'https://raw.githubusercontent.com/atomjoy/proton/main/public/images/icon-tag.png' }}" class="proton-tags-icon" alt="Icon" style="display: inline; height: 45px; width: auto; margin: 0px;">
+    <img src="https://raw.githubusercontent.com/atomjoy/proton/main/public/images/icon-tag.png" class="proton-tags-icon" alt="Icon" style="display: inline; height: 45px; width: auto; margin: 0px;">
 </x-proton::tags.div>
