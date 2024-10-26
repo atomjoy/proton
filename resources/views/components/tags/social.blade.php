c57f208755945ba1a78f88a85d6304886e45a6e5
Atomjoy <atomjoy.official@gmail.com>
2024-10-26 14:31:33 +0200

    Add files via upload

--- resources/views/components/tags/social.blade.php
+++ resources/views/components/tags/social.blade.php
@@ -1,5 +1,5 @@
 <div class="proton-social" style="text-align: center; float: left; width: 25%; box-sizing: border-box;">
     <a href="{{ request()->getSchemeAndHttpHost() }}/social/{{ $name }}" target="_blank" title="{{ $name }}">
-        <img src="/images/social-{{ $name }}.png" alt="{{ $name }}" width="30px" style="display: inline-block">
+        <img src="https://raw.githubusercontent.com/atomjoy/proton/main/public/images/social-{{ $name }}.png" alt="{{ $name }}" width="30px" style="display: inline-block">
     </a>
 </div>
