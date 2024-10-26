e8e69569d9f6650cabdc61e0c5eef02bca6af515
Atomjoy <atomjoy.official@gmail.com>
2024-10-26 14:43:11 +0200

    Update

--- resources/views/components/tags/promocode.blade.php
+++ resources/views/components/tags/promocode.blade.php
@@ -1,5 +1,6 @@
 <x-proton::tags.div type="promocode" {{ $attributes }}>
-    <div 
-    style="display: inline; float: left; padding: 10px 0px;">{{ __('Promotional code:') }} <strong>{{ $slot }}</strong></div> 
-    <img src="https://raw.githubusercontent.com/atomjoy/proton/main/public/images/icon-tag.png" class="proton-tags-icon" alt="Icon" style="display: inline; height: 45px; width: auto; margin: 0px;">
+    <p>
+        <div style="display: inline; float: left; padding: 10px 0px;">{{ __('Promotional code:') }} <strong>{{ $slot }}</strong></div> 
+        <img src="https://raw.githubusercontent.com/atomjoy/proton/main/public/images/icon-tag.png" class="proton-tags-icon" alt="Icon" style="display: inline; height: 45px; width: auto; margin: 0px;">
+    </p>
 </x-proton::tags.div>
