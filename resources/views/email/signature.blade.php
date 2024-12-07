fc2a15eeccd0e53b9be74314c5e8788c30840e24
Atomjoy <atomjoy.official@gmail.com>
2024-12-07 14:01:18 +0100

    Email signature
    Html email signature thunderbird gmail, outlook, yahoo, interia, wp, o2 ...

--- resources/views/email/signature.blade.php
+++ resources/views/email/signature.blade.php
@@ -1,169 +1,67 @@
 <!DOCTYPE html>
-<html lang="{{ $locale ?? 'pl' }}">
+<html lang="pl">
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>{{ $title ?? 'Email signature' }}</title>
-
-	<link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-
-	<style>
-		.si-body {
-			display: block;
-			padding: 0px;
-			margin: 0px;
-		}
-
-		.si-wrapper {
-			width: 100%;
-			display: block;
-			overflow: hidden;
-			box-sizing: border-box;
-			color: #222;
-			background: #f2f2fd;
-			font-size: 18px;
-			font-weight: normal;
-			font-family: 'Baloo 2', 'Open Sans', 'Roboto', 'Segoe UI', 'Helvetica Neue', Helvetica, Tahoma, Arial, monospace, sans-serif;
-		}
-
-		.si-table a.proton-link {
-			display: inline-block;
-			width: auto !important;
-			outline: none !important;
-			text-decoration: none !important;
-		}
-
-		.si-border {
-			width: 640px;
-			margin: 50px auto;
-			border: 1px solid #191715;
-			border-radius: 20px;
-			overflow: hidden;
-		}
-
-		.si-table {
-			border-collapse: collapse;
-			border-spacing: 0px;
-			border: 0px;
-			width: 640px;
-			margin: 0px auto;
-			background-color: #F1EBE4;
-			font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-			border-top-width: 3px;
-		}
-
-		.si-table td,
-		.si-table th {
-			padding: 10px 20px;
-			border-bottom: 1px solid #191715;
-		}
-
-		.si-foto {
-			display: block;
-			height: 170px;
-			width: 150px;
-			object-fit: cover;
-			border: 4px solid #ff4400;
-			box-shadow: 0px 0px 0px 4px #ff4400aa, 0px 0px 0px 8px #ff440055;
-			border-radius: 20px;
-			transition: all 1s;
-		}
-
-		.si-name {
-			color: #191715;
-			font-weight: 900;
-			font-size: 25px;
-			border-left: 1px solid #191715;
-		}
-
-		.si-title {
-			text-align: left;
-			border-left: 1px solid #191715;
-		}
-
-		.si-title strong {
-			color: #ff4400;
-			font-weight: 700;
-			font-size: 19px;
-		}
-
-		.si-row {
-			text-align: left;
-			border-left: 1px solid #191715;
-		}
-
-		.si-row strong {
-			font-weight: 600;
-			font-size: 19px;
-		}
-
-		.si-social {
-			background: #191715;
-			color: #F1EBE4;
-			padding: 15px;
-			border: 0px;
-		}
-
-		.si-icon {
-			float: left;
-			height: 30px;
-			width: 30px;
-			margin-right: 10px;
-
-		}
-
-		.si-company img {
-			float: left;
-			width: 150px;
-		}
-	</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Email signature</title>
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+    <style>
+        .si-wrapper * {
+            font-family: 'Baloo 2', 'Google Sans', 'Open Sans', Roboto, 'Segoe UI', 'Helvetica Neue', Helvetica, Tahoma, Arial, monospace, sans-serif;
+        }
+    </style>
 </head>
 
-<body class="si-body">
-	<div class="si-wrapper">
-		<div class="si-border">
-			<table class="si-table">
-				<tbody>
-					<tr>
-						<td class="si-company" colspan="2">
-							<img src="https://www.freepnglogos.com/uploads/black-netflix-logo-png-4.png" alt="Logo">
-						</td>
-					</tr>
-					<tr class="si-image">
-						<td rowspan="4" width="150px">
-							<img class="si-foto" src="https://img.freepik.com/free-photo/fashion-boy-with-yellow-jacket-blue-pants_71767-96.jpg?w=740&t=st=1687603414~exp=1687604014~hmac=e996a267d60cb3bb9467733a6fb6a5116c49020aa25efb3610f90fcb9ae83de9" alt="Foto">
-							{{-- <img class="si-foto" src="https://xsgames.co/randomusers/avatar.php?g=female" alt="Foto"> --}}
-							{{-- <img class="si-foto" src="https://i.pravatar.cc/150" alt="Foto"> --}}
-							</th>
-						<td class="si-name">Alex Cosmonova</th>
-					</tr>
-					<tr>
-						<td class="si-title"><strong>Web Developer</strong></td>
-					</tr>
-					<tr>
-						<td class="si-row"><strong>Netflix Novatec Ltd.</strong></td>
-					</tr>
-					<tr>
-						<td class="si-row">
-							<div>Email: <strong>alex@astralio.xyz</strong></div>
-							<div>Phone: <strong>+48 100 200 300</strong></div>
-						</td>
-					</tr>
-					<tr>
-						<td colspan="2" class="si-social">
-							<a href="" class="proton-link"><img class="si-icon" src="https://img.icons8.com/?size=128&id=16713&format=png"></a>
-							<a href="" class="proton-link"><img class="si-icon" src="https://img.icons8.com/?size=128&id=13963&format=png"></a>
-							<a href="" class="proton-link"><img class="si-icon" src="https://img.icons8.com/?size=128&id=32323&format=png"></a>
-							<a href="" class="proton-link"><img class="si-icon" src="https://img.icons8.com/?size=128&id=19318&format=png"></a>
-							<a href="" class="proton-link"><img class="si-icon" src="https://img.icons8.com/?size=128&id=Wkmw3VcFPr8T&format=png"></a>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
+<body class="si-body" style="display: block; padding: 0px; margin: 0px; font-family: 'Baloo 2', 'Open Sans', 'Roboto', 'Segoe UI', 'Helvetica Neue', Helvetica, Tahoma, Arial, monospace, sans-serif;">
+    <div class="si-wrapper" style="color: #222; background: #f5f4f2; font-size: 18px; font-weight: normal; display: block; overflow: hidden; box-sizing: border-box; background-position: center; background-size: cover;">
+        <div class="si-border" style="border: 1px solid #000; background: #F1EBE4; width: 600px; margin: 20px 20px; border-radius: 20px;  overflow: hidden;">
+            <div class="si-table" style="border: 0px; width: 640px; margin: 0px auto;">
+                <div class="si-company" style="border-bottom: 1px solid #000; float: left; width: 100%; padding: 15px;">
+                    <a href="https://netflix.com">
+                        <img src="https://www.freepnglogos.com/uploads/black-netflix-logo-png-4.png" style="float: left; height: 50px !important; min-height: 50px !important; max-height: 50px !important;" height="50px">
+                    </a>
+                </div>
+                <div class="si-details" style="float: left; width: 100%;">
+                    <div class=" si-image" style="float: left; width: 160px; height: auto; overflow: hidden; padding: 30px 20px 30px 20px; border-right: 1px solid #000;">
+                        <div style="display: block; height: 170px; width: 150px; border: 4px solid #ff4400; box-shadow: 0px 0px 0px 4px #ff4400aa, 0px 0px 0px 8px #ff440055; border-radius: 20px; overflow: hidden;">
+                            <img class="si-foto" src="https://xsgames.co/randomusers/avatar.php?g=female" style="display: block; height: 170px; width: 150px; object-fit: cover;">
+                        </div>
+                    </div>
+
+                    <div class="si-about" style="float: left; width: 400px; border: 0px;">
+                        <div class="si-name" style="padding: 10px 15px; color: #000; font-weight: 900; font-size: 21px; border-bottom: 1px solid #000;">
+                            Alex Cosmonova
+                        </div>
+                        <div class="si-title" style="padding: 10px 15px; border-bottom: 1px solid #000;">
+                            <strong style="color: #ff4400; font-weight: 700; font-size: 17px;">
+                                Web Developer
+                            </strong>
+                        </div>
+                        <div class="si-row" style="padding: 10px 15px; border-bottom: 1px solid #000;">
+                            <strong style="color: #000; font-weight: 700; font-size: 16px;">
+                                Netflix Novatec Ltd.
+                            </strong>
+                        </div>
+                        <div class="si-row" style="padding: 10px 15px;">
+                            <div>Email: <strong style="font-weight: 600; font-size: 16px;">
+                                    <a href="mailto:alex@astralio.xyz" target="_blank" style="color: #000; text-decoration: none;">alex@astralio.xyz</a>
+                                </strong></div>
+                            <div>Phone: <strong style="font-weight: 600; font-size: 16px;">
+                                    <a href="tel:+48100200300" target="_blank" style="color: #000; text-decoration: none;">+48 100 200 300</a></strong></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="si-social" style="float: left; width: 100%; background: #000; padding: 15px; border: 0px; box-sizing: border-box;">
+                    <a href="https://facebook.com" target="_blank" class="proton-link" style="float: left; width: auto; width: auto !important; outline: none !important; text-decoration: none !important;"><img class="si-icon" src="https://raw.githubusercontent.com/atomjoy/signatures/refs/heads/main/img/social-white/facebook.png" style="display: block; width: 40px;"></a>
+                    <a href="https://x.com" target="_blank" class="proton-link" style="float: left; width: auto; width: auto !important; outline: none !important; text-decoration: none !important;"><img class="si-icon" src="https://raw.githubusercontent.com/atomjoy/signatures/refs/heads/main/img/social-white/twitter.png" style="display: block; width: 40px;"></a>
+                    <a href="https://linkedin.com" target="_blank" class="proton-link" style="float: left; width: auto; width: auto !important; outline: none !important; text-decoration: none !important;"><img class="si-icon" src="https://raw.githubusercontent.com/atomjoy/signatures/refs/heads/main/img/social-white/linkedin.png" style="display: block; width: 40px;"></a>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
