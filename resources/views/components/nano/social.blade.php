40cd679eb1c5d32f5be7dd6c761c947b00e8a42b
Atomjoy <atomjoy.official@gmail.com>
2025-05-23 11:35:04 +0200

    Add files via upload

--- resources/views/components/nano/social.blade.php
+++ resources/views/components/nano/social.blade.php
@@ -1,27 +1,53 @@
 <x-proton::row>
-    <center>
-        <a href="{{ request()->getSchemeAndHttpHost() . '/social/facebook' }}" class="nano-icon" target="_blank">
-            <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/facebook-40.png" alt="logo">
-        </a>
+    <div class="nano-padding-inline">
+        <center>
+            @if (!empty(config('proton.social.facebook')))
+            <a href="{{ request()->getSchemeAndHttpHost() . '/social/facebook' }}" class="nano-icon" target="_blank">
+                <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/facebook-40.png" alt="logo">
+            </a>
+            @endif
 
-        <a href="{{ request()->getSchemeAndHttpHost() . '/social/x' }}" class="nano-icon" target="_blank">
-            <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/x-40.png" alt="logo">
-        </a>
+            @if (!empty(config('proton.social.x')))
+            <a href="{{ request()->getSchemeAndHttpHost() . '/social/x' }}" class="nano-icon" target="_blank">
+                <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/x-40.png" alt="logo">
+            </a>
+            @endif
 
-        <a href="{{ request()->getSchemeAndHttpHost() . '/social/youtube' }}" class="nano-icon" target="_blank">
-            <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/youtube-40.png" alt="logo">
-        </a>
+            @if (!empty(config('proton.social.linkedin')))
+            <a href="{{ request()->getSchemeAndHttpHost() . '/social/linkedin' }}" class="nano-icon" target="_blank">
+                <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/linkedin-40.png" alt="logo">
+            </a>
+            @endif
 
-        <a href="{{ request()->getSchemeAndHttpHost() . '/social/linkedin' }}" class="nano-icon" target="_blank">
-            <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/linkedin-40.png" alt="logo">
-        </a>
+            @if (!empty(config('proton.social.youtube')))
+            <a href="{{ request()->getSchemeAndHttpHost() . '/social/youtube' }}" class="nano-icon" target="_blank">
+                <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/youtube-40.png" alt="logo">
+            </a>
+            @endif
 
-        <a href="{{ request()->getSchemeAndHttpHost() . '/social/instagram' }}" class="nano-icon" target="_blank">
-            <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/instagram-40.png" alt="logo">
-        </a>
+            @if (!empty(config('proton.social.instagram')))
+            <a href="{{ request()->getSchemeAndHttpHost() . '/social/instagram' }}" class="nano-icon" target="_blank">
+                <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/instagram-40.png" alt="logo">
+            </a>
+            @endif
 
-        <a href="{{ request()->getSchemeAndHttpHost() . '/social/behance' }}" class="nano-icon" target="_blank">
-            <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/behance-40.png" alt="logo">
-        </a>
-    </center>
+            @if (!empty(config('proton.social.behance')))
+            <a href="{{ request()->getSchemeAndHttpHost() . '/social/behance' }}" class="nano-icon" target="_blank">
+                <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/behance-40.png" alt="logo">
+            </a>
+            @endif
+
+            @if (!empty(config('proton.social.dribbble')))
+            <a href="{{ request()->getSchemeAndHttpHost() . '/social/dribbble' }}" class="nano-icon" target="_blank">
+                <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/dribbble-40.png" alt="logo">
+            </a>
+            @endif
+
+            @if (!empty(config('proton.social.pinterest')))
+            <a href="{{ request()->getSchemeAndHttpHost() . '/social/pinterest' }}" class="nano-icon" target="_blank">
+                <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/pinterest-40.png" alt="logo">
+            </a>
+            @endif
+        </center>
+    </div>
 </x-proton::row>
