9c1dfeb954d4acbaf3bd7c5c117a7089754b0750
Atomjoy <atomjoy.official@gmail.com>
2025-05-23 13:30:07 +0200

    Add files via upload

--- src/Mail/Nano/FullMail.php
+++ src/Mail/Nano/FullMail.php
@@ -14,14 +14,16 @@ class FullMail extends Mailable
     public $user = null;
     public $text = null;
     public $url = null;
+    public $code = null;
     public $password = null;
 
-    public function __construct(?User $user, $text, $url = '', $password = '123456')
+    public function __construct(?User $user, $text, $url = '', $password = '', $code = '')
     {
         $this->user = $user;
         $this->text = $text;
         $this->url = $url;
         $this->password = $password;
+        $this->code = $code;
     }
 
     public function build()
