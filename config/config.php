55c93a53daf8cc45746737fd89534392af6146ac
Atomjoy <atomjoy.official@gmail.com>
2025-05-23 11:19:38 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
-	'password_image_url' => 'https://raw.githubusercontent.com/atomjoy/proton/main/public/proton-password.png',
-	'register_image_url' => 'https://raw.githubusercontent.com/atomjoy/proton/main/public/proton-register.png',
-	'f2a_image_url' => 'https://raw.githubusercontent.com/atomjoy/proton/main/public/proton-default.png',
+    'password_image_url' => 'https://raw.githubusercontent.com/atomjoy/proton/main/public/proton-password.png',
+    'register_image_url' => 'https://raw.githubusercontent.com/atomjoy/proton/main/public/proton-register.png',
+    'f2a_image_url' => 'https://raw.githubusercontent.com/atomjoy/proton/main/public/proton-default.png',
+    'default_image_url' => 'https://raw.githubusercontent.com/atomjoy/proton/main/public/proton-default.png',
+    'footer_image_url' => 'https://raw.githubusercontent.com/atomjoy/proton/main/public/logo-proton-grey.png',
 ];
