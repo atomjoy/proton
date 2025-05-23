8a7b5cba32d4c6fe7712b6dabc3b2d56077b0d3e
Atomjoy <atomjoy.official@gmail.com>
2025-05-23 11:21:42 +0200

    Add files via upload

--- resources/views/components/nano/regards.blade.php
+++ resources/views/components/nano/regards.blade.php
@@ -1,7 +1,7 @@
 <x-proton::row>
     <div class="nano-padding-inline">
-        <div class="nano-title-small">@lang('proton.f2a.regards_title')</div>
-        <div class="nano-p">@lang('proton.f2a.regards_text')</div>
+        <div class="nano-title-small">@lang('proton.regards_title')</div>
+        <div class="nano-p">@lang('proton.regards_text')</div>
 
         <div class="nano-divider"></div>
     </div>
--- resources/views/components/nano/social.blade.php
+++ resources/views/components/nano/social.blade.php
@@ -1,27 +1,27 @@
 <x-proton::row>
     <center>
-        <a href="https://example.com/social/facebook" class="nano-icon" target="_blank">
+        <a href="{{ request()->getSchemeAndHttpHost() . '/social/facebook' }}" class="nano-icon" target="_blank">
             <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/facebook-40.png" alt="logo">
         </a>
 
-        <a href="https://example.com/social/behance" class="nano-icon" target="_blank">
-            <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/behance-40.png" alt="logo">
-        </a>
-
-        <a href="https://example.com/social/x" class="nano-icon" target="_blank">
+        <a href="{{ request()->getSchemeAndHttpHost() . '/social/x' }}" class="nano-icon" target="_blank">
             <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/x-40.png" alt="logo">
         </a>
 
-        <a href="https://example.com/social/instagram" class="nano-icon" target="_blank">
-            <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/instagram-40.png" alt="logo">
+        <a href="{{ request()->getSchemeAndHttpHost() . '/social/youtube' }}" class="nano-icon" target="_blank">
+            <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/youtube-40.png" alt="logo">
         </a>
 
-        <a href="https://example.com/social/linkedin" class="nano-icon" target="_blank">
+        <a href="{{ request()->getSchemeAndHttpHost() . '/social/linkedin' }}" class="nano-icon" target="_blank">
             <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/linkedin-40.png" alt="logo">
         </a>
 
-        <a href="https://example.com/social/youtube" class="nano-icon" target="_blank">
-            <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/youtube-40.png" alt="logo">
+        <a href="{{ request()->getSchemeAndHttpHost() . '/social/instagram' }}" class="nano-icon" target="_blank">
+            <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/instagram-40.png" alt="logo">
+        </a>
+
+        <a href="{{ request()->getSchemeAndHttpHost() . '/social/behance' }}" class="nano-icon" target="_blank">
+            <img src="https://raw.githubusercontent.com/atomjoy/icons/refs/heads/main/black/behance-40.png" alt="logo">
         </a>
     </center>
 </x-proton::row>
