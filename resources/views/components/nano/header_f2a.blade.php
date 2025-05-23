4c2e031072f6c16ec58c70d902d4103e8acb2494
Atomjoy <atomjoy.official@gmail.com>
2025-05-23 13:06:58 +0200

    Add files via upload

---
+++ resources/views/components/nano/header_f2a.blade.php
@@ -0,0 +1,3 @@
+<x-proton::row class="nano-padding">
+    <img src="{{ config('proton.f2a_image_url', 'https://raw.githubusercontent.com/atomjoy/proton/main/public/proton-default.png') }}" alt="Image">
+</x-proton::row>
---
+++ resources/views/components/nano/header_password.blade.php
@@ -0,0 +1,3 @@
+<x-proton::row class="nano-padding">
+    <img src="{{ config('proton.password_image_url', 'https://raw.githubusercontent.com/atomjoy/proton/main/public/proton-default.png') }}" alt="Image">
+</x-proton::row>
