66f900cc476783dea18f0354e8bf83b957e2f225
Atomjoy <atomjoy.official@gmail.com>
2025-07-07 20:19:24 +0200

    Update social.blade.php

--- resources/views/components/social.blade.php
+++ resources/views/components/social.blade.php
@@ -1,3 +1,3 @@
 <a href="{{ request()->getSchemeAndHttpHost() }}/social/{{ $name }}" class="proton-link proton-social-link" target="_blank" title="{{ $name }}">
-	<img src="https://raw.githubusercontent.com/atomjoy/proton/main/public/social/{{ $name }}.png" alt="Image">
+	<img src="https://raw.githubusercontent.com/atomjoy/proton/main/public/social/{{ $name }}.png" alt="Image" width="35px" height="35px">
 </a>
